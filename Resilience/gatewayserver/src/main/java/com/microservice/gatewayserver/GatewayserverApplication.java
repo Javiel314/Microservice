d1eed1fa7f35d824598a4f65a564ab1b17beec6b
Javiel314 <javiel.le.fi@gmail.com>
2025-10-06 19:36:45 +0200

    added failback to gatway server

--- Resilience/gatewayserver/src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
+++ Resilience/gatewayserver/src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
@@ -22,7 +22,8 @@ public class GatewayserverApplication {
 						.path("/sofibank/accounts/**")
 						.filters(f -> f.rewritePath("/sofibank/accounts/(?<segment>.*)", "/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")))
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
 						.uri("lb://ACCOUNTS"))
 				.route(p-> p
 						.path("/sofibank/loans/**")
